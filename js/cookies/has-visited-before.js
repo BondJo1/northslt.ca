e9e0b1afeb0606e218db35cc0d1d4023543f0f72
Joel Bond <Joel.Bond@sterling.education>
2018-10-30 13:10:21 -0600

    Update has-visited-before.js

--- js/cookies/has-visited-before.js
+++ js/cookies/has-visited-before.js
@@ -1,9 +1,10 @@
 var hasVisited = Cookies.get('hasVisited');
-if ( hasVisited != null ) {
+if (hasVisited != null) {
     Cookies.set('hasVisited', 'true', { expires: 365 });
     console.log("The hasVisited cookie has been set, value is " + hasVisited)
 }
 else {
     //$('#firstvisit').css({'visibility':'hidden'});
+    $("#firstvisit").css("visibility", "hidden");
     console.log("The hasVisited cookie is already created, value is " + hasVisited)
 }
