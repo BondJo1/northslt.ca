9728c45c8e70d62ef44b9d394a810c0d257ca127
Joel Bond <Joel.Bond@sterling.education>
2018-10-30 11:50:20 -0600

    Add a space after "is"

--- js/cookies/cookie-test.js
+++ js/cookies/cookie-test.js
@@ -1,8 +1,8 @@
 var testCookie = Cookies.get('test');
 if ( testCookie != null ) {
     Cookies.set('test', 'testing cookiejs', { expires: 365 });
-    console.log("Test cookie has been created, value is" + testCookie)
+    console.log("Test cookie has been created, value is " + testCookie)
 }
 else {
-    console.log("Cookie is already created, value is" + testCookie)
+    console.log("Cookie is already created, value is " + testCookie)
 }
