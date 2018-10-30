00c5b91eb8bf1ff095e16ee5ee44add7337536ba
Joel Bond <Joel.Bond@sterling.education>
2018-10-30 12:04:42 -0600

    Create has-visited-before.js, update ref in index.html

---
+++ js/cookies/has-visited-before.js
@@ -0,0 +1,8 @@
+var hasVisited = Cookies.get('hasVisited');
+if ( hasVisited != null ) {
+    Cookies.set('hasVisited', 'true', { expires: 365 });
+    console.log("The hasVisited cookie has been set, value is " + hasVisited)
+}
+else {
+    console.log("The hasVisited cookie is already created, value is " + hasVisited)
+}
