d7463e34f47a5ca628200d8234d14549ed12431a
Joel Bond <Joel.Bond@sterling.education>
2018-10-31 09:48:03 -0600

    Update cookie-test.js, fix refs

--- js/cookies/cookie-test.js
+++ js/cookies/cookie-test.js
@@ -1,6 +1,7 @@
 var testCookie = Cookies.get('test');
 if ( testCookie != null ) {
     Cookies.set('test', 'testing cookiejs', { expires: 365 });
+    var testCookie = Cookies.get('test');
     console.log("Test cookie has been created, value is " + testCookie)
 }
 else {
