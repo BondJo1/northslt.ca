9ce9d65e846fcb511e1a3cf3d266e7a4676927f6
Joel Bond <Joel.Bond@sterling.education>
2018-10-31 09:11:38 -0600

    Update the code.... again

--- js/cookies/has-visited-before.js
+++ js/cookies/has-visited-before.js
@@ -1,6 +1,8 @@
 var hasVisited = Cookies.get('hasVisited');
 if (hasVisited != null) {
-    document.getElementById('firstvisit').classList.add('is-active');
+    //document.getElementById('firstvisit').classList.add('is-active');
+    var modal = document.querySelector('.modal');
+    modal.classList.add('is-active');
     Cookies.set('hasVisited', 'true', { expires: 365 });
     console.log("The hasVisited cookie has been set, value is " + hasVisited)
 }
