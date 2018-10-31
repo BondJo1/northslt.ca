85745f5056e3cb4ad45a965dd781f77a8879acbc
Joel Bond <Joel.Bond@sterling.education>
2018-10-31 09:19:36 -0600

    Fix the variable declarations

--- js/cookies/has-visited-before.js
+++ js/cookies/has-visited-before.js
@@ -5,8 +5,10 @@ if (hasVisited != undefined) {
     modal.classList.add('is-active');
     html.classList.add('is-clipped')
     Cookies.set('hasVisited', 'true', { expires: 365 });
+    var hasVisited = Cookies.get('hasVisited')
     console.log("The hasVisited cookie has been set, value is " + hasVisited)
 }
 else {
+    var hasVisited = Cookies.get('hasVisited')
     console.log("The hasVisited cookie is already created, value is " + hasVisited)
 }
