01cc5602b08266891edfff42ab2c170c92d4cfdb
Joel Bond <Joel.Bond@sterling.education>
2018-10-31 09:18:38 -0600

    Update the if statement

--- js/cookies/has-visited-before.js
+++ js/cookies/has-visited-before.js
@@ -1,14 +1,12 @@
 var hasVisited = Cookies.get('hasVisited');
-//if (hasVisited != null) {
-    //document.getElementById('firstvisit').classList.add('is-active');
+if (hasVisited != undefined) {
     var modal = document.querySelector('.modal');  // assuming you have only 1
     var html = document.querySelector('html');
     modal.classList.add('is-active');
     html.classList.add('is-clipped')
     Cookies.set('hasVisited', 'true', { expires: 365 });
     console.log("The hasVisited cookie has been set, value is " + hasVisited)
-//}
-//else {
-    //document.getElementById("firstvisit").style.visibility = "hidden";
+}
+else {
     console.log("The hasVisited cookie is already created, value is " + hasVisited)
-//}
+}
