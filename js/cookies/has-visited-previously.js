f6ea193dcfd9afe798c765515c6303b37eebfaad
Joel Bond <Joel.Bond@sterling.education>
2018-10-31 10:33:27 -0600

    Create has-visited-previously with new cookie code

---
+++ js/cookies/has-visited-previously.js
@@ -0,0 +1,12 @@
+if (Cookie.exists('hasVisited')) {
+    console.log("The hasVisited cookie is already created, value is " + hasVisited)
+}
+else {
+    var modal = document.querySelector('.modal');  // assuming you have only 1
+    var html = document.querySelector('html');
+    modal.classList.add('is-active');
+    html.classList.add('is-clipped')
+    Cookie.set('hasVisited', 'true', { expires: 365 });
+    var hasVisited = Cookie.get('hasVisited')
+    console.log("The hasVisited cookie has been set, value is " + hasVisited)
+}
