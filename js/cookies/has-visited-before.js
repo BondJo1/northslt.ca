ff00f7d6f753fea45aaf0fdd7145e270a4ac88dd
Joel Bond <Joel.Bond@sterling.education>
2018-10-31 09:18:06 -0600

    Add is-clipped to <html>

--- js/cookies/has-visited-before.js
+++ js/cookies/has-visited-before.js
@@ -1,12 +1,14 @@
 var hasVisited = Cookies.get('hasVisited');
 //if (hasVisited != null) {
     //document.getElementById('firstvisit').classList.add('is-active');
-    var modal = document.querySelector('.modal');
+    var modal = document.querySelector('.modal');  // assuming you have only 1
+    var html = document.querySelector('html');
     modal.classList.add('is-active');
+    html.classList.add('is-clipped')
     Cookies.set('hasVisited', 'true', { expires: 365 });
     console.log("The hasVisited cookie has been set, value is " + hasVisited)
 //}
 //else {
     //document.getElementById("firstvisit").style.visibility = "hidden";
-//    console.log("The hasVisited cookie is already created, value is " + hasVisited)
+    console.log("The hasVisited cookie is already created, value is " + hasVisited)
 //}
