4579621449476406f8c5358c96c6d8046c4a903d
Joel Bond <Joel.Bond@sterling.education>
2018-10-31 09:13:40 -0600

    Remove the if statement to see if that fixes the issue

--- js/cookies/has-visited-before.js
+++ js/cookies/has-visited-before.js
@@ -1,12 +1,12 @@
 var hasVisited = Cookies.get('hasVisited');
-if (hasVisited != null) {
+//if (hasVisited != null) {
     //document.getElementById('firstvisit').classList.add('is-active');
     var modal = document.querySelector('.modal');
     modal.classList.add('is-active');
     Cookies.set('hasVisited', 'true', { expires: 365 });
     console.log("The hasVisited cookie has been set, value is " + hasVisited)
-}
-else {
+//}
+//else {
     //document.getElementById("firstvisit").style.visibility = "hidden";
-    console.log("The hasVisited cookie is already created, value is " + hasVisited)
-}
+//    console.log("The hasVisited cookie is already created, value is " + hasVisited)
+//}
