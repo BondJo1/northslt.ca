e94f1e28270e795bad675d4a5b89db02cc1630c9
Joel Bond <Joel.Bond@sterling.education>
2018-10-31 10:46:15 -0600

    Update has-visited-previously.js

--- js/cookies/has-visited-previously.js
+++ js/cookies/has-visited-previously.js
@@ -2,10 +2,20 @@ if (Cookie.exists('hasVisited')) {
     console.log("The hasVisited cookie is already created, value is " + hasVisited)
 }
 else {
-    var modal = document.querySelector('.modal');  // assuming you have only 1
+    var modal = document.querySelector('.modal');
     var html = document.querySelector('html');
     modal.classList.add('is-active');
     html.classList.add('is-clipped')
+    modal.querySelector('.modal-background').addEventListener('click', function (e) {
+        e.preventDefault();
+        modal.classList.remove('is-active');
+        html.classList.remove('is-clipped');
+    });
+    modal.querySelector('.modal-close.is-large').addEventListener('click', function (e) {
+        e.preventDefault();
+        modal.classList.remove('is-active');
+        html.classList.remove('is-clipped');
+    });
     Cookie.set('hasVisited', 'true', {
         expires: 365
     });
