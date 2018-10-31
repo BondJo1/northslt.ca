c4025604dfbafd46a57c7679e13a2194d650109a
Joel Bond <Joel.Bond@sterling.education>
2018-10-31 09:04:57 -0600

    Remove the visibility:hidden tag, add a classList.add('is-ective');

--- js/cookies/has-visited-before.js
+++ js/cookies/has-visited-before.js
@@ -1,11 +1,10 @@
 var hasVisited = Cookies.get('hasVisited');
 if (hasVisited != null) {
-    firstvisit.classList.toggle('is-active')
+    document.getElementById('firstvisit').classList.add('is-active');
     Cookies.set('hasVisited', 'true', { expires: 365 });
     console.log("The hasVisited cookie has been set, value is " + hasVisited)
 }
 else {
-    //$('#firstvisit').css({'visibility':'hidden'});
-    document.getElementById("firstvisit").style.visibility = "hidden";
+    //document.getElementById("firstvisit").style.visibility = "hidden";
     console.log("The hasVisited cookie is already created, value is " + hasVisited)
 }
