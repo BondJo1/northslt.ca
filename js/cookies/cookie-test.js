1f236cd25c3379cecd8945a823973e0aa34024ad
Joel Bond <Joel.Bond@sterling.education>
2018-10-31 11:16:15 -0600

    Remove unneeded js files

--- js/cookies/cookie-test.js
+++
@@ -1,9 +0,0 @@
-var testCookie = Cookies.get('test');
-if ( testCookie != null ) {
-    Cookies.set('test', 'testing cookiejs', { expires: 365 });
-    var testCookie = Cookies.get('test');
-    console.log("Test cookie has been created, value is " + testCookie)
-}
-else {
-    console.log("Cookie is already created, value is " + testCookie)
-}
