ee3ffd016e646ab5be005430349428690fd11fd7
Joel Bond <Joel.Bond@sterling.education>
2018-11-02 11:08:57 -0600

    Totally change code for the Contact Us button

--- js/homepage.js
+++ js/homepage.js
@@ -105,23 +105,18 @@ function userViews() {
 }
 
 function contactUsModal() {
-    var modal = document.querySelector('#contact-us-modal');
-    var html = document.querySelector('html');
-    var hero = document.querySelector('hero-body');
-    modal.querySelector('#survey-modal-close').addEventListener('click', function (e) {
-        e.preventDefault();
-        modal.classList.remove('is-active');
-        html.classList.remove('is-clipped');
-    });
-    modal.querySelector('.modal-background').addEventListener('click', function (e) {
-        e.preventDefault();
-        modal.classList.remove('is-active');
-        html.classList.remove('is-clipped');
-    });
     document.querySelector('a#open-modal').addEventListener('click', function (event) {
         event.preventDefault();
+        var modal = document.querySelector('#contact-us-modal');  // assuming you have only 1
+        var html = document.querySelector('html');
         modal.classList.add('is-active');
         html.classList.add('is-clipped');
+
+        modal.querySelector('.modal-background').addEventListener('click', function (e) {
+            e.preventDefault();
+            modal.classList.remove('is-active');
+            html.classList.remove('is-clipped');
+        });
     });
 }
 
