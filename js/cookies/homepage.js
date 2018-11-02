291b557f6e6cfd183fe93a58f2cd5998e035c455
sltdev04 <joeltbond04@gmail.com>
2018-11-01 19:01:57 -0600

    Create homepage.js to call all JS files for the homepage

---
+++ js/cookies/homepage.js
@@ -0,0 +1,7 @@
+const hasVisited = require('./has-visited-previously.js');
+const surveyNotification = require('./survey-notification.js');
+const userViews = require('./user-views.js');
+
+hasVisited();
+surveyNotification();
+userViews();
