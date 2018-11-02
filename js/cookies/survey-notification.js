d6847a88ee5e3f8242c5c6e3c335ca25b7f3d90a
Joel Bond <Joel.Bond@sterling.education>
2018-11-02 09:49:01 -0600

    Remove classes that aren't defined correctly

--- js/cookies/survey-notification.js
+++ js/cookies/survey-notification.js
@@ -11,18 +11,14 @@ if (Cookie.exists('userViews')) {
 
 function activateSurveyModal() {
     var modal = document.querySelector('#survey-modal');
-    var html = document.querySelector('html');
     modal.classList.add('is-active');
-    html.classList.add('is-clipped')
     modal.querySelector('.modal-close').addEventListener('click', function (e) {
         e.preventDefault();
         modal.classList.remove('is-active');
-        html.classList.remove('is-clipped');
     });
     modal.querySelector('.modal-background').addEventListener('click', function (e) {
         e.preventDefault();
         modal.classList.remove('is-active');
-        html.classList.remove('is-clipped');
     });
     Cookie.set('surveyCompleted', true, {
         expires: 365
