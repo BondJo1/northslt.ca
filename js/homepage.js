22c58b32b08152948fd288cd59c9dd108d6ffe23
sltdev04 <joeltbond04@gmail.com>
2018-11-02 21:43:24 -0600

    Rename homepage-beta.js to homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -1,131 +1,140 @@
 function surveyModal() {
-    if (Cookie.exists('userViews')) {
-        if (Cookie.get('userViews') > 9) {
-            if (Cookie.exists('surveyCompleted')) {
-                logAlreadyDone();
-            }
-            else {
-                setSurveyCookie();
-                activateSurveyModal();
-                logCompleted();
-            }
-        }
+  if (localStorage.userViews) {
+    if (localStorage.userViews > 9) {
+      if (localStorage.surveyCompleted) {
+        logAlreadyDone();
+      } else {
+        setSurveyCookie();
+        activateSurveyModal();
+        logCompleted();
+      }
     }
+  }
+}
 
-    function logAlreadyDone() {
-        console.log('You have already completed the survey, thanks!');
-    }
+function logAlreadyDone() {
+  console.log("You have already completed the survey, thanks!");
+}
 
-    function logCompleted() {
-        console.log('Survey has been shown, and surveyCompleted cookie has been set.');
-    }
+function logCompleted() {
+  console.log(
+    "Survey has been shown, and surveyCompleted cookie has been set."
+  );
+}
 
-    function setSurveyCookie() {
-        Cookie.set('surveyCompleted', true, {
-            expires: 365
-        });
-    }
+function setSurveyCookie() {
+  localStorage.surveyCompleted = true;
+}
 
-    function activateSurveyModal() {
-        var modal = document.querySelector('#survey-modal');
-        var html = document.querySelector('html');
-        modal.classList.add('is-active');
-        html.classList.add('is-clipped')
-        modal.querySelector('#survey-modal-close').addEventListener('click', function (e) {
-            e.preventDefault();
-            modal.classList.remove('is-active');
-            html.classList.remove('is-clipped');
-        });
-        modal.querySelector('.modal-background').addEventListener('click', function (e) {
-            e.preventDefault();
-            modal.classList.remove('is-active');
-            html.classList.remove('is-clipped');
-        });
-    }
+function activateSurveyModal() {
+  var modal = document.querySelector("#survey-modal");
+  var html = document.querySelector("html");
+  modal.classList.add("is-active");
+  html.classList.add("is-clipped");
+  modal
+    .querySelector("#survey-modal-close")
+    .addEventListener("click", function(e) {
+      e.preventDefault();
+      modal.classList.remove("is-active");
+      html.classList.remove("is-clipped");
+    });
+  modal
+    .querySelector(".modal-background")
+    .addEventListener("click", function(e) {
+      e.preventDefault();
+      modal.classList.remove("is-active");
+      html.classList.remove("is-clipped");
+    });
 }
 
 function hasVisited() {
-    if (Cookie.exists('hasVisited')) {
-        console.log("The hasVisited cookie is already created.")
-    }
-    else {
-        setVisitedCookie();
-        activateWelcomeModal();
-        console.log("The hasVisited cookie has been created.")
-    }
+  if (localStorage.hasVisited) {
+    console.log("The hasVisited cookie is already created.");
+  } else {
+    setVisitedCookie();
+    activateWelcomeModal();
+    console.log("The hasVisited cookie has been created.");
+  }
 
-    function setVisitedCookie() {
-        Cookie.set('hasVisited', 'true', {
-            expires: 365
-        });
-    }
+  function setVisitedCookie() {
+    localStorage.hasVisited = true;
+  }
 
-    function activateWelcomeModal() {
-        var modal = document.querySelector('#firstvisit');
-        var html = document.querySelector('html');
-        modal.classList.add('is-active');
-        html.classList.add('is-clipped')
-        modal.querySelector('.modal-close').addEventListener('click', function (e) {
-            e.preventDefault();
-            modal.classList.remove('is-active');
-            html.classList.remove('is-clipped');
-        });
-        modal.querySelector('.modal-background').addEventListener('click', function (e) {
-            e.preventDefault();
-            modal.classList.remove('is-active');
-            html.classList.remove('is-clipped');
-        });
-    }
+  function activateWelcomeModal() {
+    var modal = document.querySelector("#firstvisit");
+    var html = document.querySelector("html");
+    modal.classList.add("is-active");
+    html.classList.add("is-clipped");
+    modal.querySelector(".modal-close").addEventListener("click", function(e) {
+      e.preventDefault();
+      modal.classList.remove("is-active");
+      html.classList.remove("is-clipped");
+    });
+    modal
+      .querySelector(".modal-background")
+      .addEventListener("click", function(e) {
+        e.preventDefault();
+        modal.classList.remove("is-active");
+        html.classList.remove("is-clipped");
+      });
+  }
 }
 
 function userViews() {
-    if (Cookie.exists('userViews')) {
-        updateViews();
-    }
-    else {
-        createViews();
-    }
+  if (localStorage.userViews) {
+    updateViews();
+  } else {
+    createViews();
+  }
 
-    function createViews() {
-        Cookie.set('userViews', 1, {
-            expires: 365
-        });
-        console.log('Thanks for visiting! We have set your userViews to 1.');
-    }
+  function createViews() {
+    localStorage.userViews = 1;
+    console.log("Thanks for visiting! We have set your userViews to 1.");
+  }
 
-    function updateViews() {
-        var currentViews = Cookie.get('userViews');
-        var addCurrentView = +currentViews + 1;;
-        Cookie.set('userViews', addCurrentView, {
-            expires: 365
-        });
-        var updatedViews = Cookie.get('userViews');
-        console.log('userViews value has now been updated, you have visited the website ' + updatedViews + ' times.');
-    }
+  function updateViews() {
+    var currentViews = localStorage.userViews;
+    var addCurrentView = +currentViews + 1;
+    localStorage.userViews = addCurrentView;
+    var updatedViews = localStorage.userViews;
+    console.log(
+      "userViews value has now been updated, you have visited the website " +
+        updatedViews +
+        " times."
+    );
+  }
 }
 
 function contactUsModal() {
-    document.querySelector('a#open-modal').addEventListener('click', function (event) {
-        event.preventDefault();
-        var modal = document.querySelector('#contact-us-modal');  // assuming you have only 1
-        var html = document.querySelector('html');
-        modal.classList.add('is-active');
-        html.classList.add('is-clipped');
+  document
+    .querySelector("a#open-modal")
+    .addEventListener("click", function(event) {
+      event.preventDefault();
+      var modal = document.querySelector("#contact-us-modal"); // assuming you have only 1
+      var html = document.querySelector("html");
+      modal.classList.add("is-active");
+      html.classList.add("is-clipped");
 
-        modal.querySelector('.modal-background').addEventListener('click', function (e) {
-            e.preventDefault();
-            modal.classList.remove('is-active');
-            html.classList.remove('is-clipped');
+      modal
+        .querySelector(".modal-background")
+        .addEventListener("click", function(e) {
+          e.preventDefault();
+          modal.classList.remove("is-active");
+          html.classList.remove("is-clipped");
         });
-        modal.querySelector('#close-contact-modal').addEventListener('click', function (e) {
-            e.preventDefault();
-            modal.classList.remove('is-active');
-            html.classList.remove('is-clipped');
+      modal
+        .querySelector("#close-contact-modal")
+        .addEventListener("click", function(e) {
+          e.preventDefault();
+          modal.classList.remove("is-active");
+          html.classList.remove("is-clipped");
         });
-        modal.querySelector('#contact-aria-close').addEventListener('click', function (e) {
-            e.preventDefault();
-            modal.classList.remove('is-active');
-            html.classList.remove('is-clipped');
+      modal
+        .querySelector("#contact-aria-close")
+        .addEventListener("click", function(e) {
+          e.preventDefault();
+          modal.classList.remove("is-active");
+          html.classList.remove("is-clipped");
         });
     });
 }
