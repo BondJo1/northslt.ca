b14fc8f57851f6c74a56db4c236ab1e5de06f521
Joel Bond <Joel.Bond@sterling.education>
2018-11-06 10:09:29 -0600

    Update the campus-page.js file with some new code for the sidebar

--- js/campus-page.js
+++ js/campus-page.js
@@ -1,14 +1,15 @@
-$(function importBX() {
-    $('.image-slider').bxSlider({
-        mode: 'fade',
-        captions: true,
-        slideWidth: 600
-    });
+$('.image-slider').bxSlider({
+    mode: 'fade',
+    captions: true,
+    slideWidth: 600
 });
+
+$('#scrollto').click(function() {
+    $('.sidebar').removeClass('is-active');
+    $('.menu-icon-wrapper').removeClass('open');
+})
 
 function scroll(element) {
     var ele = document.getElementById(element);
     window.scrollTo(ele.offsetLeft, ele.offsetTop);
 }
-
-importBX();
