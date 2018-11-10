1993eb788e6116a8688800320ecbb8bd2f9ee0a3
Joel Bond <joeltbond04@gmail.com>
2018-11-09 21:33:56 -0600

    Remove unnecessary comments

--- js/homepage.js
+++ js/homepage.js
@@ -110,7 +110,7 @@ function contactUsModal() {
     .querySelector("a#open-modal")
     .addEventListener("click", function(event) {
       event.preventDefault();
-      var modal = document.querySelector("#contact-us-modal"); // assuming you have only 1
+      var modal = document.querySelector("#contact-us-modal");
       var html = document.querySelector("html");
       modal.classList.add("is-active");
       html.classList.add("is-clipped");
